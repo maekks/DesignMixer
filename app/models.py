d9eb2d2bd0dd911b54421447a5584eddcd244af2
maekks <hi@maxlee.im>
2016-05-01 15:26:16 -0400

    add basic profile

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from . import login_manager, db
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from flask import current_app
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from datetime import datetime
+
 
 class Permission:
     FOLLOW = 0x01
@@ -53,6 +55,12 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+
 
     #constructor of the User class
     def __init__(self, **kwargs):
@@ -100,6 +108,10 @@ class User(UserMixin, db.Model):
 
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
 
     def __repr__(self):
         return '<User %r>' % self.username
