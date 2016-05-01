e688951c057b168bce3f62d8f4c59382dfb75ba4
maekks <hi@maxlee.im>
2016-05-01 16:18:01 -0400

    add edit profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,3 +9,9 @@ from ..models import Role, User
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
+
+class EditProfileForm(Form):
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
