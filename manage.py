396f37c3fb16560f38890d907f1b2d9f58f41f34
max <hi@maxlee.im>
2016-05-30 12:00:10 -0400

    update database

--- manage.py
+++ manage.py
@@ -48,7 +48,8 @@ def test(coverage=False):
 def deploy():
     from flask.ext.migrate import upgrade
     from app.models import Role, User
-
+    from app import db
+    db.create_all()
     upgrade()
 
     Role.insert_roles()
