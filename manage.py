58f22befb49f17eaadcb97bf3a8678799927e703
max <hi@maxlee.im>
2016-05-30 11:52:42 -0400

    fix database problems

--- manage.py
+++ manage.py
@@ -46,12 +46,11 @@ def test(coverage=False):
 
 @manager.command
 def deploy():
-    """run deployment tasks."""
     from flask.ext.migrate import upgrade
     from app.models import Role, User
 
     upgrade()
-    
+
     Role.insert_roles()
     User.add_self_follows()
 
